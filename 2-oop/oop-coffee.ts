83dff3719d09d5c8c74a3dc0d36aac465e1239f4
cho-hyeonjin <gusls2005@gmail.com>
2024-04-22 12:16:47 +0900

    ♻️refactor: class contained static property and static method

--- 2-oop/oop-coffee.ts
+++ 2-oop/oop-coffee.ts
@@ -5,18 +5,25 @@
   };
 
   class CoffeeMaker {
-    BEANS_GRAMM_PER_SHOT: number = 7;
+    /** Class Lv Property --- 인스턴스마다 만들어지지 않음 ∴ 메모리 낭비 방지 */
+    static BEANS_GRAMM_PER_SHOT: number = 7;
+    /** Instance Lv (member) Property */
     coffeeBeansGR: number = 0;
 
     constructor(coffeeBeansGR: number) {
       this.coffeeBeansGR = coffeeBeansGR;
     }
 
+    /** Class Lv Method */
+    static makeCoffeeMaker(coffeeBeansGR: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeansGR);
+    }
+
     makeCoffee(shots: number): CoffeeCup {
-      if (this.coffeeBeansGR < shots * this.BEANS_GRAMM_PER_SHOT) {
+      if (this.coffeeBeansGR < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
         throw new Error("😰 Not enough coffee beans! 🫘");
       }
-      this.coffeeBeansGR -= this.BEANS_GRAMM_PER_SHOT * shots;
+      this.coffeeBeansGR -= CoffeeMaker.BEANS_GRAMM_PER_SHOT * shots;
       return {
         shots: shots,
         hasMilk: false,
@@ -28,4 +35,7 @@
   const coffeeMakerB = new CoffeeMaker(200);
   console.log(coffeeMakerA);
   console.log(coffeeMakerB);
+
+  const coffeeMakerC = CoffeeMaker.makeCoffeeMaker(300);
+  console.log(coffeeMakerC);
 }
